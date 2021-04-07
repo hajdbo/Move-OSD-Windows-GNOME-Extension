ad16e6b1d231f6f58da74bf3098d9273b048c61f
RuiGuilherme <guilhermecunha.rui@gmail.com>
2021-04-07 12:10:11 -0300

    GTK4 and Gnome Shell 40 Support

--- prefs.js
+++ prefs.js
@@ -13,107 +13,110 @@ const Convenience = Me.imports.convenience;
 //-----------------------------------------------
 
 function init() {
-    Convenience.initTranslations();
+Convenience.initTranslations();
 }
 
 //-----------------------------------------------
 
-const OSDSettingsWidget = new GObject.Class({
-    Name: 'OSD.Prefs.Widget',
-    GTypeName: 'OSDPrefsWidget',
-    Extends: Gtk.Box,
+const OSDSettingsWidget = GObject.registerClass({
+Name: 'OSD.Prefs.Widget',
+GTypeName: 'OSDPrefsWidget'
+}, class OSDSettingsWidget extends Gtk.Box {
 
-    _init: function(params) {
-	this.parent(params);
-        this.margin = 30;
-        this.spacing = 25;
-        this.fill = true;
-        this.set_orientation(Gtk.Orientation.VERTICAL);
+_init(params) {
+	super._init(params);
+	let frame = new Gtk.Box({
+		orientation: Gtk.Orientation.VERTICAL,
+		margin_top: 30,
+		margin_bottom: 30,
+		margin_start: 30,
+		margin_end: 30,
+		spacing: 25
+	});
 
 	this._settings = Convenience.getSettings('org.gnome.shell.extensions.better-osd');
 		
 	let labelHorizontalPercentage = _("Horizontal position (percentage) :");
 		
 	let horizontalPercentage = new Gtk.SpinButton();
-        horizontalPercentage.set_sensitive(true);
-        horizontalPercentage.set_range(-60, 60);
+		horizontalPercentage.set_sensitive(true);
+		horizontalPercentage.set_range(-60, 60);
 	horizontalPercentage.set_value(0);
-        horizontalPercentage.set_value(this._settings.get_int('horizontal'));
-        horizontalPercentage.set_increments(1, 2);
-        
+		horizontalPercentage.set_value(this._settings.get_int('horizontal'));
+		horizontalPercentage.set_increments(1, 2);
+		
 	horizontalPercentage.connect('value-changed', Lang.bind(this, function(w){
 		var value = w.get_value_as_int();
 		this._settings.set_int('horizontal', value);
 	}));
 		
 	let hBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
-	hBox.pack_start(new Gtk.Label({ label: labelHorizontalPercentage, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
-	hBox.pack_end(horizontalPercentage, false, false, 0);
-	this.add(hBox);
-		
+	hBox.prepend(new Gtk.Label({ label: labelHorizontalPercentage, use_markup: true, halign: Gtk.Align.START }));
+	hBox.append(horizontalPercentage);
+	frame.append(hBox);
 	//-------------------------------------------------------
 		
 	let labelVerticalPercentage = _("Vertical position (percentage) :");
 		
 	let verticalPercentage = new Gtk.SpinButton();
-        verticalPercentage.set_sensitive(true);
-        verticalPercentage.set_range(-110, 110);
+		verticalPercentage.set_sensitive(true);
+		verticalPercentage.set_range(-110, 110);
 	verticalPercentage.set_value(70);
 	verticalPercentage.set_value(this._settings.get_int('vertical'));
-        verticalPercentage.set_increments(1, 2);
-        
+		verticalPercentage.set_increments(1, 2);
+		
 	verticalPercentage.connect('value-changed', Lang.bind(this, function(w){
 		var value = w.get_value_as_int();
 		this._settings.set_int('vertical', value);
 	}));
 		
 	let vBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
-	vBox.pack_start(new Gtk.Label({ label: labelVerticalPercentage, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
-	vBox.pack_end(verticalPercentage, false, false, 0);
-	this.add(vBox);
-		
+	vBox.prepend(new Gtk.Label({ label: labelVerticalPercentage, use_markup: true, halign: Gtk.Align.START }));
+	vBox.append(verticalPercentage);
+	frame.append(vBox);
+
 	//-------------------------------------------------------
 		
 	let labelSizePercentage = _("Size (percentage) :");
 		
 	let sizePercentage = new Gtk.SpinButton();
-        sizePercentage.set_sensitive(true);
-        sizePercentage.set_range(0, 100);
+		sizePercentage.set_sensitive(true);
+		sizePercentage.set_range(0, 100);
 	sizePercentage.set_value(20);
 	sizePercentage.set_value(this._settings.get_int('size'));
-        sizePercentage.set_increments(1, 2);
-        
+		sizePercentage.set_increments(1, 2);
+		
 	sizePercentage.connect('value-changed', Lang.bind(this, function(w){
 		var value = w.get_value_as_int();
 		this._settings.set_int('size', value);
 	}));
 		
 	let sizeBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
-	sizeBox.pack_start(new Gtk.Label({ label: labelSizePercentage, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
-	sizeBox.pack_end(sizePercentage, false, false, 0);
-	this.add(sizeBox);
-		
+	sizeBox.prepend(new Gtk.Label({ label: labelSizePercentage, use_markup: true, halign: Gtk.Align.START }));
+	sizeBox.append(sizePercentage);
+	frame.append(sizeBox);
 	//-------------------------------------------------------
 		
 	let labelDelay = _("Hide Delay (ms) :");
 		
 	let hideDelay = new Gtk.SpinButton();
-        hideDelay.set_sensitive(true);
-        hideDelay.set_range(0, 5000);
+		hideDelay.set_sensitive(true);
+		hideDelay.set_range(0, 5000);
 	hideDelay.set_value(1500);
 	hideDelay.set_value(this._settings.get_int('delay'));
-        hideDelay.set_increments(1, 2);
-        
+		hideDelay.set_increments(1, 2);
+		
 	hideDelay.connect('value-changed', Lang.bind(this, function(w){
 		var value = w.get_value_as_int();
 		this._settings.set_int('delay', value);
 	}));
 		
 	let delayBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 15 });
-	delayBox.pack_start(new Gtk.Label({ label: labelDelay, use_markup: true, halign: Gtk.Align.START }), false, false, 0);
-	delayBox.pack_end(hideDelay, false, false, 0);
-	this.add(delayBox);
-    }
+	delayBox.prepend(new Gtk.Label({ label: labelDelay, use_markup: true, halign: Gtk.Align.START }));
+	delayBox.append(hideDelay);
+	frame.append(delayBox);
+	return frame;
+}
 });
 
 //-----------------------------------------------
@@ -121,8 +124,5 @@ const OSDSettingsWidget = new GObject.Class({
 //I guess this is like the "enable" in extension.js : something called each
 //time he user try to access the settings' window
 function buildPrefsWidget() {
-    let widget = new OSDSettingsWidget();
-    widget.show_all();
-
-    return widget;
+return new OSDSettingsWidget();
 }
